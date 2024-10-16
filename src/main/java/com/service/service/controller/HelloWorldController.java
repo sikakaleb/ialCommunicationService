e33aefb75789adb3880a659112b03bf87f591954
bilal-mesaouri <bilmsr.bm@gmail.com>
2024-10-16 21:49:51 +0200

    adding kubernetes chart + database connection

--- src/main/java/com/service/service/controller/HelloWorldController.java
+++ src/main/java/com/service/service/controller/HelloWorldController.java
@@ -1,13 +1,27 @@
 package com.service.service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.service.service.model.User;
+import com.service.service.service.UserService;
 
 @RestController
 public class HelloWorldController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, World!";
+        // Hardcoded user data
+        User user = new User();
+        user.setName("John Doe");
+        user.setEmail("john.doe@examplev.com");
+
+        // Save the user to the database
+        userService.saveUser(user);
+        return "turkish hallo";
     }
 }
